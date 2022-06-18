a3a9140e994368fd7baf1d1e7f21a021514eb39b
Andy Chow <cesiumhydrogen@gmail.com>
2022-06-18 11:56:34 -0700

    Revert "Remove files from jetbrains"
    This reverts commit 910451d38ba6fc9a69d13e1cd7458abac91f501d.

---
+++ cmake-build-debug/CMakeFiles/kaleidoscope.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kaleidoscope.dir/my_lang.cpp.obj"
+  "kaleidoscope.exe"
+  "kaleidoscope.exe.manifest"
+  "kaleidoscope.pdb"
+  "libkaleidoscope.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kaleidoscope.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
